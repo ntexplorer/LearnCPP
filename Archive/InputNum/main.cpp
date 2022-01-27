75f825d37125a3941a5668f14da8706ce2c62529
ntexplorer <ntexplorer@qq.com>
2022-01-27 17:01:41 +0800

    Chapter 1.5 Completed.

---
+++ Archive/InputNum/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Please enter a number:" << std::endl;
+    int x{};
+    std::cin >> x;
+    std::cout << "The number you entered is: " << x << "\n";
+    return 0;
+}
